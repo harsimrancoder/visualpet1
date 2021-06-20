d2f84ace503b46d70b21419cbdd491f40e51625a
harsimrancoder <78964799+harsimrancoder@users.noreply.github.com>
2021-06-20 19:15:47 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,8 @@
+class Food{
+constructor(){
+    this.FoodStocks=0;
+    this.image=loadImage("images/Milk.png");
+    this.lastFed;
+}
+
+ }
--- sketch.js
+++ sketch.js
@@ -1,22 +1,62 @@
-//Create variables here
+var database
+var dog,happyDog
+var foodS,foodStock
 
 function preload()
 {
-	//load images here
+	dog1=loadImage("images/dogImg.png")
+  happyDog=loadImage('images/dogImg1.png')
 }
 
 function setup() {
-	createCanvas(800, 700);
+	createCanvas(500, 500);
   
+  database=firebase.database()
+  foodstock=database.ref('Food');
+  foodstock.on("value",readStock); 
+
+  dog=createSprite(250,300,150,150)
+  dog.addImage(dog1)
+  dog.scale=0.2
 }
 
 
 function draw() {  
+background(46,139,87);
 
-  drawSprites();
-  //add styles here
 
+
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS);
+  dog.addImage(happyDog);
 }
 
 
 
+
+
+  drawSprites();
+  //add styles here
+  textSize(25)
+  fill ("white")
+  text("food remaining= "+foodS,170,200)
+
+}
+
+function readStock(data){
+  foodS=data.val();
+
+}
+function writeStock(x){
+if(x<=0){
+  x=0;
+}else{
+  x=x-1;
+}
+
+
+
+  database.ref('/').update({
+  Food:x
+  })
+}
